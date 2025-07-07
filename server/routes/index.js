5f3fb1613198143f02578c2ab8ab703a58a946e4
Wajed Isleem <wajedbisleem@gmail.com>
2025-07-07 01:32:08 -0600

    Refactor database connection to use sequelize instead of pool

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,12 +4,12 @@ const router = express.Router();
 // empty for assignment 10.1, Add code below for assignment 10.2
 
 
-const pool = require('../config/db');
+const {sequelize} = require('../config/db');
 
 router.get('/', async (req, res) => {
   try {
-    const result = await pool.query('SELECT NOW()');
-    res.send(`Connected: ${result.rows[0].now}`);
+    const result = await sequelize.query('SELECT NOW()');
+    res.send(`Connected: ${result[0][0].now}`);
   } catch (err) {
     res.status(500).send('Database connection failed');
   }
